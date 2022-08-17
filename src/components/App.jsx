92ce936767bc66046328445f8c3f3cf1a238f3d0
megomax88 <huku.ukuk@yandex.ru>
2022-08-17 16:06:09 +0300

    auth rychki

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>hello</h1>} />
+      <Route path="/" element={<h1>hello</h1>} />
+
     </Routes>
   );
 }
+<Route path="/" element={<h1>hello</h1>} />
