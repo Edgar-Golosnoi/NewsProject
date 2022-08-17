5fc53f73b3be73322941c37acd22478318d59f5b
Maxximiiian <SantoCastor@yandex.ru>
2022-08-17 16:05:02 +0300

    +-

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import AuthPage from './Auth/AuthPage';
+import Registration from './Auth/Registration';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>hello</h1>} />
+      <Route path="/" element={<AuthPage />} />
+      <Route path="/registration" element={<Registration />} />
     </Routes>
   );
 }
